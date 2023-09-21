ef576b5c25ed2fccebeb5b13988292f040731c79
Nattabude <nattabude.t2021@gmail.com>
2023-09-21 10:40:20 +0700

    try

--- app.py
+++ app.py
@@ -4,14 +4,6 @@ import numpy as np
 import json
 from sklearn.preprocessing import PolynomialFeatures
 
-def create_app():
-    app = Flask(__name__)
-    
-    # Add any configuration or routes here
-    
-    return app
-
-app = create_app()
 
 class predictor:
 
@@ -115,66 +107,73 @@ for k,v in model_info.items():
         model_info[k]["input"] = json.load(file)
 
 
-@app.route('/')
-def main_home():
-    return render_template('home.html')
+def create_app():
 
-@app.route('/<model_name>', methods=['GET', 'POST'])
-def ml_predict(model_name):
-    error_message = None
+    app = Flask(__name__)
 
-    if request.method == 'POST':
-        input_values = []
+    @app.route('/')
+    def main_home():
+        return render_template('home.html')
 
-        for input_info in model_info[model_name]["input"]:
-            input_name = input_info["name"]
-            input_type = input_info["type"]
-            user_input = request.form.get(input_name)
+    @app.route('/<model_name>', methods=['GET', 'POST'])
+    def ml_predict(model_name):
+        error_message = None
 
-            if user_input == '':
-                user_input = input_info["default"]
-            try:
-                # Convert input to the specified type
-                if input_type == "float":
-                    user_input = float(user_input)
-                elif input_type == "int":
-                    user_input = int(user_input)
+        if request.method == 'POST':
+            input_values = []
 
-                input_values.append(user_input) 
+            for input_info in model_info[model_name]["input"]:
+                input_name = input_info["name"]
+                input_type = input_info["type"]
+                user_input = request.form.get(input_name)
 
-                if input_name =="year" and user_input < 1886:
-                    return render_template('result.html', 
-                                        car_price='Did you know? first car in the world born in 1886 known as the "Motorwagen"', 
-                                        input_values=input_values,
-                                            model_input=model_info[model_name]["input"],
-                                            sender=model_name)
-      
-            except:
-                error_message = f"Invalid input for {input_name}. Please enter a valid number."
-                break
+                if user_input == '':
+                    user_input = input_info["default"]
+                try:
+                    # Convert input to the specified type
+                    if input_type == "float":
+                        user_input = float(user_input)
+                    elif input_type == "int":
+                        user_input = int(user_input)
+
+                    input_values.append(user_input) 
+
+                    if input_name =="year" and user_input < 1886:
+                        return render_template('result.html', 
+                                            car_price='Did you know? first car in the world born in 1886 known as the "Motorwagen"', 
+                                            input_values=input_values,
+                                                model_input=model_info[model_name]["input"],
+                                                sender=model_name)
         
-        if error_message:
-            return render_template('index.html', 
-                                   model_input=model_info[model_name]["input"] ,
-                                   describe= model_info[model_name]["describe"], 
-                                   page_style = model_info[model_name]["page_style"], 
-                                   error_message=error_message)
-        
-
-
-        # Predict car price using both 
-        car_price = model_info[model_name]["prediction"].predict(model_info[model_name],input_values)
+                except:
+                    error_message = f"Invalid input for {input_name}. Please enter a valid number."
+                    break
             
-        return render_template('result.html', 
-                               car_price=car_price, 
-                               input_values=input_values,
-                                 model_input=model_info[model_name]["input"],
-                                 sender=model_name)
+            if error_message:
+                return render_template('index.html', 
+                                    model_input=model_info[model_name]["input"] ,
+                                    describe= model_info[model_name]["describe"], 
+                                    page_style = model_info[model_name]["page_style"], 
+                                    error_message=error_message)
+            
 
-    return render_template('index.html', 
-                           model_input=model_info[model_name]["input"] ,
-                           describe= model_info[model_name]["describe"], 
-                           page_style = model_info[model_name]["page_style"])
+
+            # Predict car price using both 
+            car_price = model_info[model_name]["prediction"].predict(model_info[model_name],input_values)
+                
+            return render_template('result.html', 
+                                car_price=car_price, 
+                                input_values=input_values,
+                                    model_input=model_info[model_name]["input"],
+                                    sender=model_name)
+
+        return render_template('index.html', 
+                            model_input=model_info[model_name]["input"] ,
+                            describe= model_info[model_name]["describe"], 
+                            page_style = model_info[model_name]["page_style"])
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(host="0.0.0.0",port=80)
