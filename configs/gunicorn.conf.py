69190dd209c30525609a0219cbcf6b1ef8336be9
Nattabude <nattabude.t2021@gmail.com>
2025-09-01 06:24:31 +0000

    refactor: update Gunicorn configuration for worker and thread settings combine publish and deploy step for git action

--- configs/gunicorn.conf.py
+++ configs/gunicorn.conf.py
@@ -1,5 +1,8 @@
 import os
 port = os.getenv("PORT", "90")
 bind = f"0.0.0.0:{port}"
-workers = os.getenv("WEB_CONCURRENCY", "4")
+workers = int(os.getenv("WEB_CONCURRENCY", "2"))
+threads = int(os.getenv("GUNICORN_THREADS", "2"))
 timeout = 120
+
+
